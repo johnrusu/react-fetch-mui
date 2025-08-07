106ab3b149f9a3b00fd7bd48baa831bb90da84bf
Rusu Ionut <johnrusu@gmail.com>
2025-08-07 23:42:29 +0300

    feat: fixes

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-import { title } from "process";
+// src/constants.ts
+// Constants for API URL and button texts
 
 const API_URL = "https://dog.ceo/api/breeds/image/random/20";
 const BUTTON_TEXTS = {
